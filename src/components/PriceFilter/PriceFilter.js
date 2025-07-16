1ff181c31e658f07b504910b1d2b0e2eb0c059bd
nigelnh <121311174+nigelnh@users.noreply.github.com>
2025-07-16 23:48:32 +0700

    Update price format

--- src/components/PriceFilter/PriceFilter.js
+++ src/components/PriceFilter/PriceFilter.js
@@ -1,9 +1,9 @@
 function PriceFilter({ priceFilter, onFilterChange }) {
   const priceOptions = [
     { value: "all", label: "Tất cả mức giá" },
-    { value: "under500k", label: "Dưới 500,000 VND" },
-    { value: "500k-1m", label: "500,000 - 1,000,000 VND" },
-    { value: "over1m", label: "Trên 1,000,000 VND" },
+    { value: "under500k", label: "< 500.000 đ" },
+    { value: "500k-1m", label: "500K - 1M đ" },
+    { value: "over1m", label: "> 1.000.000 đ" },
   ];
 
   return (
